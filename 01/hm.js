6756549605fd0a9af78bcdb6e21a15af296ef18c
VikaDomets <dometswat@icloud.com>
2024-10-30 19:59:24 +0200

    new

---
+++ 01/hm.js
@@ -0,0 +1,21 @@
+let userNickname = 'UserNick';
+console.log(userNickname);
+
+let userScore = 25;
+
+let age = 22;
+console.log(age);
+age += 5;
+
+console.log(age);
+let isStudent = true;
+console.log(isStudent);
+isStudent = false;
+console.log(isStudent);
+
+let item = 8;
+console.log(item);
+item = 'ten';
+console.log(item);
+item = true;
+console.log(item);
